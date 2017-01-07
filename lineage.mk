e7119250b33a88faa6e3d62d487258061b1ecb01
firebird11 <hbgassel@gmail.com>
2017-01-07 11:34:21 +0100

    d802: rebrand to Lineage
    Change-Id: Iae89c9007d65d7a776236dae66a1abfd2a83d8c9

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d802
-PRODUCT_NAME := cm_d802
+PRODUCT_NAME := lineage_d802
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
